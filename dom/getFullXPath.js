fa5ba65645e6a9fa9fd47dc9567716d294c20a3c
Karim <84965867+abkarim@users.noreply.github.com>
2023-01-26 13:48:01 +0600

    Update and rename element-to-xpath.js to getFullXPath.js

---
+++ dom/getFullXPath.js
@@ -0,0 +1,36 @@
+function getFullXPath(element) {
+    let selector = '';
+    let isRoot;
+    let currentElement = element;
+
+    do {
+        const tagName = currentElement.tagName.toLowerCase();
+        const parentElement = currentElement.parentElement;
+
+        if (parentElement.childElementCount > 1) {
+            const parentsChildren = [...parentElement.children];
+            let tag = [];
+            parentsChildren.forEach(child => {
+                if (child.tagName.toLowerCase() === tagName) tag.push(child) // Append to tag
+            })
+
+            if (tag.length === 1) {
+                selector = `/${tagName}${selector}`;
+            } else {
+                const position = tag.indexOf(currentElement) + 1;
+                selector = `/${tagName}[${position}]${selector}`;
+            }
+
+        } else {
+            //* Current element has no siblings
+            selector = `/${tagName}${selector}`;
+        }
+
+        currentElement = parentElement;
+        isRoot = parentElement.tagName.toLowerCase() === 'html';
+        if(isRoot) selector = `/html${selector}`;
+    }
+    while (isRoot === false);
+
+    return selector;
+}
